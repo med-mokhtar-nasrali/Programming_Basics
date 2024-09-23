e2d1a5bc3d93c3fe95d55684aac76c74c2567c6e
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2024-09-23 20:11:17 +0100

    new

---
+++ Day1/coding.js
@@ -0,0 +1,7 @@
+// This will count the numbers of likes
+var likeCount = 0;
+// this will increase the like by the amount written
+function increaseLikes() {
+    // this will increase the likes by only 1 like
+    likeCount = likeCount + 1;
+}
