d324f3eeb817522b93bf389de821b65331c27829
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2024-09-24 15:33:36 +0100

    jspredict

---
+++ Day_2/Practice/Nasrali_Med_Mokhtar_JSPredict.js
@@ -0,0 +1,3 @@
+//Predict 1 : it will print again 
+//Predict 2 :it will print the born place and birth year that he put in input function  
+//Predict 3 : consol.log 2 will print "num1 is 10" and console.log 3 will print "num2 is 20"
---
+++ Day_2/Practice/Nasrali_Med_Mokhtar_LoopDLoop.js
@@ -0,0 +1,21 @@
+// we need a loop to count how many candies they got
+// the starting point is on 0 miles var miles = 0
+// the loop stops when they reach mile 6
+// the loop always will compare the miles they run and when it get 6 miles will stop
+// the loop will go by 2 steps
+// var miles = 0 
+
+var candies = 0
+
+for (var miles = 2 ; miles <= 6 ; miles = miles + 2) {
+    
+
+        console.log("They Got",candies + 1,"Candy");
+    }
+
+// This will print they got 1 candy 3 times
+
+
+
+
+
