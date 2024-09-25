a8c03bb27488933f6e11c6d4d384360410687a95
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2024-09-25 14:35:34 +0100

    fixed_operators

--- Day_2/Practice/Nasrali_Med_Mokhtar_Operators.js
+++ Day_2/Practice/Nasrali_Med_Mokhtar_Operators.js
@@ -1,15 +1,21 @@
 function howMuchLeftOverCake(numberOfPieces,numberOfPeople){
 
-    var leftovers = numberOfPieces % numberOfPeople;
+    var leftovers = numberOfPieces - numberOfPeople;
 
-if(leftovers === 0){
+if(leftovers <= 0){
     console.log("No leftovers for you!") //this will print No leftovers for you! if the leftovers == 0
 }else if (leftovers <= 2){
+    
     console.log("You have some leftovers") // this will print You have some leftovers if there is leftovers between 1 and 2
+    console.log("number of leftovers",leftovers)
+
 }else if (leftovers <=5){
+    console.log("number of leftovers",leftovers)
     console.log("You have leftovers to share") // this will print You have leftovers to share if there is leftovers between 3 and 5
-}else { 
-    console.log("Hold another party!") //this will print hold another party if there is no leftovers
+}else{
+    (numberOfPieces >= 5)
+    console.log("number of leftovers",leftovers)
+    console.log("Hold another party!")
 }
     return leftovers; //this will save the leftovers value for later
 }
