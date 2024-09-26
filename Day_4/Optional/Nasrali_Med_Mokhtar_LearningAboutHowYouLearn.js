0541da13e97ef8999fea65b34041f3bd4de5d593
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2024-09-26 13:25:33 +0100

    answers

---
+++ Day_4/Optional/Nasrali_Med_Mokhtar_LearningAboutHowYouLearn.js
@@ -0,0 +1,5 @@
+//1: i've learned many tricks during these 4 days and im waiting for
+//more and more to get , know i can read the lines of code and guess almost what is every line is going to do
+//2: For sure the program is exhausting but it is worth it , learning never end 
+//3/ Never reading the code so fast i need to go line by line to know what is it doing
+//i've read many of parameters and Uppercases and operators the wrong way
